813eba2f7ae9cefad627713b2eb5d24621d42985
Benjamin Armintor <armintor@gmail.com>
2013-03-19 09:13:07 -0400

    starting a testing rig

--- src/test/java/org/fcrepo/client/GetMatcher.java
+++ src/test/java/org/fcrepo/client/GetMatcher.java
@@ -8,18 +8,27 @@ import org.mockito.ArgumentMatcher;
 public class GetMatcher extends ArgumentMatcher<HttpGet> {
 	
 	private final HttpGet expected;
+	private final boolean match;
 	
-	private GetMatcher(HttpGet expected) {
+	private GetMatcher(HttpGet expected, boolean match) {
 		this.expected = expected;
+		this.match = match;
 	}
 
 	@Override
 	public boolean matches(Object argument) {
-		return expected.getURI().toString().equals(((HttpGet)argument).getURI().toString());
+		boolean result = (argument != null) 
+				? expected.getURI().toString().equals(((HttpGet)argument).getURI().toString())
+				: false;
+		return match == result;
 	}
 	
 	public static HttpGet getLike(HttpGet expected) {
-		return argThat(new GetMatcher(expected));
+		return argThat(new GetMatcher(expected, true));
+	}
+
+	public static HttpGet getNotLike(HttpGet expected) {
+		return argThat(new GetMatcher(expected, false));
 	}
 
 }
